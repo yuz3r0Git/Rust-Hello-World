e6bc0a81816d2a8d88beecb529144a19d00e2a18
yuz3r0Git <hyt.luvtnis.ad.try.everything@gmail.com>
2024-07-11 18:00:25 +0900

    Rust Hello World

---
+++ hello-rust/target/debug/deps/hello_rust-78d83ee583a981e0.d
@@ -0,0 +1,5 @@
+/Users/itforce/Documents/mygithub/Rust-Hello-World/hello-rust/target/debug/deps/hello_rust-78d83ee583a981e0: src/main.rs
+
+/Users/itforce/Documents/mygithub/Rust-Hello-World/hello-rust/target/debug/deps/hello_rust-78d83ee583a981e0.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/itforce/Documents/mygithub/Rust-Hello-World/hello-rust/target/debug/hello-rust: /Users/itforce/Documents/mygithub/Rust-Hello-World/hello-rust/src/main.rs
